419b183609f661391332b5cbb001df19a2e435b2
Jaari <jaari.rey@gmail.com>
2023-04-07 19:10:00 +1000

    small changes before reinstalling tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import TypingTestResults from "./components/TypingTestResults";
 import { TestWords } from "./interfaces/WordStructure";
 import { COMPONENT_FADE_DURATION } from "./constants/constants";
 
-	 
+
 function App() {
 	const [testWords, setTestWords] = useState<TestWords>({words: [], errorCountHard: 0, errorCountSoft: 0, timeElapsedMilliSeconds: 0, characterCount: 0, keystrokeCharacterCount: 0});
 	const [testLength, setTestLength] = useState<number>(25);
@@ -28,19 +28,20 @@ function App() {
 		<div className="App">
 			<div className="flex items-center justify-center">
 				<div className="grid grid-cols-3 gap-1 w-4/5">
+					<div className="col-span-full h-10"></div>
 					<div className={`col-span-1 text-center pr-4 select-none transition-opacity opacity-${componentOpacity} duration-${COMPONENT_FADE_DURATION}`}>
 						<TestLengthSelector testLength={testLength} setTestLength={setTestLength}/>
 					</div>
-					<div className={`col-span-1 text-center pr-4 select-none transition-opacity opacity-${componentOpacity} duration-${COMPONENT_FADE_DURATION}`}>
+					<div className={`col-span-1 text-center pr-4 transition-opacity opacity-${componentOpacity} duration-${COMPONENT_FADE_DURATION}`}>
 						<NumberSelector numbers={includeNumbers} setNumbers={setIncludeNumbers}/>
 					</div>
-					<div className={`col-span-1 text-center pl-4 select-none transition-opacity opacity-${componentOpacity} duration-${COMPONENT_FADE_DURATION}`}>
+					<div className={`col-span-1 text-center pr-4 transition-opacity opacity-${componentOpacity} duration-${COMPONENT_FADE_DURATION}`}>
 						<PunctuationSelector punctuation={includePunctuation} setPunctuation={setIncludePunctuation}/>
 					</div>
 					<div className="col-span-full bg-blue-500 rounded text-center">
 						<button onClick={() => setReset(!reset)}>Reset</button>
 					</div>
-					<div className="col-span-full select-none bg-zinc-600 rounded h-96">
+					<div className="col-span-full select-none h-96">
 						<TypingTest testWords={testWords} setTestWords={setTestWords} testLength={testLength} numbers={includeNumbers} punctuation={includePunctuation} reset={reset} setShowResultsComponent={setShowResultsComponent} testRunning={testRunning} setTestRunning={setTestRunning}/>
 					</div>
 					{showResultsComponent && 
