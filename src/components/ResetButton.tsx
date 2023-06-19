14adfc8cfe07719e27233c7340f697d748f70406
Jaari <jaari.rey@gmail.com>
2023-06-20 01:22:10 +1000

    added hover text for reset button

--- src/components/ResetButton.tsx
+++ src/components/ResetButton.tsx
@@ -1,6 +1,8 @@
-import React, {RefObject} from "react";
+import React, {RefObject, useEffect, useState} from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faRefresh } from "@fortawesome/free-solid-svg-icons";
+import { hover } from "@testing-library/user-event/dist/hover";
+import { Tooltip } from "recharts";
 
 interface Props {
 	buttonRef: RefObject<HTMLButtonElement>,
@@ -13,13 +15,28 @@ interface Props {
 const ResetButton = ({buttonRef, opacityStyle, reset, setReset, resultsComponentOpacity}: Props) => {
 
 	const spinningStyle = (resultsComponentOpacity == 1) ? "spinning-icon" : "";
+	const [toolTipOpacity, setToolTipOpacity] = useState<number>(0);
+
+	const hoverTextStyling = {
+		"--reset-button-text-opacity": toolTipOpacity,
+	} as React.CSSProperties;
+
 	return (
 		<div style={opacityStyle} className="reset-container" tabIndex={-1}>
-			<button ref={buttonRef} type="reset" title="Reset" className="reset-button"
-				onClick={() => setReset(!reset)}>
+			<button ref={buttonRef} 
+				type="reset"
+				className="reset-button"
+				onClick={() => setReset(!reset)}
+				onMouseEnter={() => setToolTipOpacity(1)}
+				onMouseLeave={() => setToolTipOpacity(0)}
+				// onFocus={() => setToolTipOpacity(1)}
+				// onBlur={() => setToolTipOpacity(0)}
+			>
 				<FontAwesomeIcon icon={faRefresh} className={`reset-icon ${spinningStyle}`}/>
 			</button>
-			
+			<div style={hoverTextStyling} className="reset-button-text">
+				reset test
+			</div>
 		</div>
 	);
 };
