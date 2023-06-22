1bcc85a64be8f8bffb90541db2656aa713632c93
Jaari <jaari.rey@gmail.com>
2023-06-23 00:06:30 +1000

    fixed tooltip for results graph

--- src/components/TypingTestResultsWPMGraph.tsx
+++ src/components/TypingTestResultsWPMGraph.tsx
@@ -14,30 +14,15 @@ interface Props {
 	averageWPMArray: NumberPair[],
 }
 
-interface AxisDomain {
-	lowerBound: number,
-	upperBound: number
-}
-
 const MyChartComponent = ({rawWPMArray, averageWPMArray}: Props) => {
 	
 	const [graphData, setGraphData] = useState<DataPoint[]>([]);
-	const [xMax, setXMax] = useState<number>();
-	const [yDomain, setYDomain] = useState<AxisDomain>({lowerBound: 0, upperBound: 0});
 
 	useEffect(() => {		
 		// combines raw and average datapoints at each interval into 1 type
 		const combinedArray = rawWPMArray.map((rawWPM, index) => {	
 			return {interval: rawWPM.interval, rawWPM: rawWPM.wpm, averageWPM: averageWPMArray[index].wpm};
 		});
-
-		// gets the y axis borders
-		const yDataPoints = combinedArray.map(valuePoint => 
-			[valuePoint.averageWPM, valuePoint.rawWPM]
-		).flat();
-		
-		setYDomain({lowerBound: Math.min(...yDataPoints) - 10, upperBound: Math.max(...yDataPoints) + 10});
-
 		setGraphData(combinedArray);	
 	}, []);
 
@@ -47,12 +32,9 @@ const MyChartComponent = ({rawWPMArray, averageWPMArray}: Props) => {
 		opacity: 0.9
 	} as React.CSSProperties;
 
-	const customTooltipLabel = (label: any) => {
-		if (label === 1) {
-		  return "Label A"; // Customize the label for data point with name 'A'
-		}
-		return label; // Return the original label for other data points
-	  };
+	const customTooltipLabel = (label: number) => { 
+		return `second: ${label}`;
+	};		
 
 	
 	return (
@@ -88,8 +70,8 @@ const MyChartComponent = ({rawWPMArray, averageWPMArray}: Props) => {
 						}}
 					/>
 					<CartesianGrid stroke="rgb(161 161 170)" strokeWidth={0.7} fill="rgb(161 161 170)" fillOpacity={0.2}/>
-					<Line type="monotone" dataKey="rawWPM" stroke="#8884d8" strokeWidth={3} dot={false}/>
-					<Line type="monotone" dataKey="averageWPM" stroke="#FFFF00" strokeWidth={4} dot={false}/>
+					<Line type="monotone" dataKey="rawWPM" name="raw" stroke="#8884d8" strokeWidth={3} dot={false}/>
+					<Line type="monotone" dataKey="averageWPM" name="average" stroke="#FFFF00" strokeWidth={4} dot={false}/>
 					<Tooltip contentStyle={toolTipStyle} cursor={false} labelFormatter={customTooltipLabel}/>
 					
 				</LineChart>
