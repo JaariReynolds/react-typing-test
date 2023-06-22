a8d515aac4a7da1a4eee8772c964d6bf55887c38
Jaari <jaari.rey@gmail.com>
2023-06-22 23:00:30 +1000

    update to the graph/results, no more background

--- src/components/TypingTestResultsWPMGraph.tsx
+++ src/components/TypingTestResultsWPMGraph.tsx
@@ -9,24 +9,50 @@ interface DataPoint {
 	averageWPM: number
 }
 
-interface IProps {
+interface Props {
 	rawWPMArray: NumberPair[],
 	averageWPMArray: NumberPair[],
 }
 
-const MyChartComponent = ({rawWPMArray, averageWPMArray}: IProps) => {
+interface AxisDomain {
+	lowerBound: number,
+	upperBound: number
+}
+
+const MyChartComponent = ({rawWPMArray, averageWPMArray}: Props) => {
 	
 	const [graphData, setGraphData] = useState<DataPoint[]>([]);
-	
+	const [xMax, setXMax] = useState<number>();
+	const [yDomain, setYDomain] = useState<AxisDomain>({lowerBound: 0, upperBound: 0});
 
-	// combines raw and average datapoints at each interval into 1 type
 	useEffect(() => {		
-		const combinedArray = rawWPMArray.map((rawWPM, index) => {
+		// combines raw and average datapoints at each interval into 1 type
+		const combinedArray = rawWPMArray.map((rawWPM, index) => {	
 			return {interval: rawWPM.interval, rawWPM: rawWPM.wpm, averageWPM: averageWPMArray[index].wpm};
 		});
+
+		// gets the y axis borders
+		const yDataPoints = combinedArray.map(valuePoint => 
+			[valuePoint.averageWPM, valuePoint.rawWPM]
+		).flat();
+		
+		setYDomain({lowerBound: Math.min(...yDataPoints) - 10, upperBound: Math.max(...yDataPoints) + 10});
+
 		setGraphData(combinedArray);	
-		console.log(combinedArray);
 	}, []);
+
+	const toolTipStyle = {
+		backgroundColor: "rgb(47, 50, 52)",
+		borderRadius: "0.5rem",
+		opacity: 0.9
+	} as React.CSSProperties;
+
+	const customTooltipLabel = (label: any) => {
+		if (label === 1) {
+		  return "Label A"; // Customize the label for data point with name 'A'
+		}
+		return label; // Return the original label for other data points
+	  };
 
 	
 	return (
@@ -34,34 +60,37 @@ const MyChartComponent = ({rawWPMArray, averageWPMArray}: IProps) => {
 			<ResponsiveContainer width="90%" height={300}>
 				<LineChart data={graphData}>
 					<XAxis 
-						tick={{fill: "rgb(47, 50, 52)"}}
-
-						type="number"
-						tickCount={23}
-						domain={[1, "dataMax"]}
+						tick={{fill: "rgb(161 161 170)"}}
+						//type="number"
+						//tickCount={xMax as number + 1}
+						
+						//domain={[1, xMax as number]}
 						dataKey="interval" 
 						height={40}
 						label={{
 							value: "Seconds", 
 							dy: 15,
-							fill:"rgb(47, 50, 52)"
+							fill:"rgb(161 161 170)"
 						}}
 					/>
 					<YAxis 
-						tick={{fill:"rgb(47, 50, 52)"}} 
+						tick={{fill:"rgb(161 161 170)"}} 
 						dataKey="rawWPM"
+						domain={[0, "dataMax + 10"]}
 						width={40}
 						label={{
 							value: "Words per Minute", 
 							dx: -20, 
 							angle: -90,
-							fill:"rgb(47, 50, 52)"
+							fill:"rgb(161 161 170)"
+							 
+							
 						}}
 					/>
-					<CartesianGrid stroke="rgb(47, 50, 52)" />
-					<Line type="monotone" dataKey="rawWPM" stroke="#8884d8" />
-					<Line type="monotone" dataKey="averageWPM" stroke="#FFFF00" />
-					<Tooltip />
+					<CartesianGrid stroke="rgb(161 161 170)" strokeWidth={0.7} fill="rgb(161 161 170)" fillOpacity={0.2}/>
+					<Line type="monotone" dataKey="rawWPM" stroke="#8884d8" strokeWidth={3} dot={false}/>
+					<Line type="monotone" dataKey="averageWPM" stroke="#FFFF00" strokeWidth={4} dot={false}/>
+					<Tooltip contentStyle={toolTipStyle} cursor={false} labelFormatter={customTooltipLabel}/>
 					
 				</LineChart>
 			</ResponsiveContainer>
