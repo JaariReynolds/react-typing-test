348d3649775804d76f1bdbf2337c092362a018a0
Jaari <jaari.rey@gmail.com>
2023-09-10 23:08:40 +1000

    basic sign up, sign in, logout working basic useContext working for user

--- src/components/Header/AccountDashboard.tsx
+++ src/components/Header/AccountDashboard.tsx
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useRef } from "react";
+import { logout } from "../../functions/account/accountFunctions";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDoorOpen } from "@fortawesome/free-solid-svg-icons";
+import { useUserContext } from "../../contexts/UserContext";
 
 const AccountDashboard = () => {
+	const user = useUserContext();
+
+	// assumes there is a user logged in 
 	return (
-		<div className="account-dashboard">
+		<div className="account-dashboard">		
             suhh
 
+			<div className="logout-button">
+				<button onClick={() => logout()}>
+					<FontAwesomeIcon icon={faDoorOpen} className="icon"/>
+					logout
+				</button>
+			</div>
 		</div>
 	);
 };
