25f4b7bcbc751209156066abec1af9f06fe8697f
Jaari <jaari.rey@gmail.com>
2023-09-12 15:24:39 +1000

    moved selectedPaletteId to userContext

--- src/contexts/UserContext.tsx
+++ src/contexts/UserContext.tsx
@@ -1,13 +1,23 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
 import React, { useState, useEffect, useContext } from "react";
 import { auth } from "../firebase";
 import { User, onAuthStateChanged } from "firebase/auth";
+import UpdateCssVariablePaletteObject from "../components/HelperComponents/UpdateCssVariablePaletteObject";
 
 
 interface UserInfo {
     user: User | null,
+	selectedPaletteId: number,
+	setSelectedPaletteId: (id: number) => void
 }
 
-export const UserContext = React.createContext<UserInfo|undefined>({user: null});
+export const UserContext = React.createContext<UserInfo|undefined>(
+	{
+		user: null,
+		selectedPaletteId: 0,
+		setSelectedPaletteId: () => {}
+	}
+);
 
 export const useUserContext = () => {
 	const user = useContext(UserContext);
@@ -19,21 +29,28 @@ export const useUserContext = () => {
 
 export const UserProvider = ({children}: any) => {
 	const [user, setUser] = useState<User|null>(null);
-	
+	const [selectedPaletteId, setSelectedPaletteId] = useState<number>(0);
+
 	useEffect(() => {
 		const unsubscribe = onAuthStateChanged(auth, (user) => {
 			if (user) {
 				setUser(user);
+				setSelectedPaletteId(5);
 			}
 			else {
 				setUser(null);
+				setSelectedPaletteId(0);
 			}
 		});
 		return unsubscribe;
 	}, []);
 
+	UpdateCssVariablePaletteObject(selectedPaletteId);
+
 	const value = {
-		user
+		user,
+		selectedPaletteId,
+		setSelectedPaletteId
 	};
 
 	return (
