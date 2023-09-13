1c4e1b64c15f1d6d52ac3444c8b855a24af8ef5f
Jaari <jaari.rey@gmail.com>
2023-09-13 14:52:57 +1000

    fixed env file, added firestore

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,8 +1,9 @@
 import {initializeApp} from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const app = initializeApp({ 
-	apiKey: "AIzaSyAwnq4np-Q-GW8fY49VqLWD5BCTJdFw54I",
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
 	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
 	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
@@ -10,5 +11,9 @@ const app = initializeApp({
 	appId: process.env.REACT_APP_FIREBASE_APP_ID,
 });
 
+
 export const auth = getAuth(app);
+export const database = getFirestore(app);
+
+
 export default app;
