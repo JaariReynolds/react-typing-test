1131beea9c4dff4b7036d1629ba80092867cbac5
Jaari <jaari.rey@gmail.com>
2023-09-22 23:01:31 +1000

    moved the user context provider to the very top

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { UserProvider } from "./contexts/UserContext";
 
 
 const root = ReactDOM.createRoot(
@@ -10,7 +11,9 @@ const root = ReactDOM.createRoot(
 );
 root.render(
 	<React.StrictMode>
-		<App />
+		<UserProvider>
+			<App />
+		</UserProvider>
 	</React.StrictMode>
 );
 
