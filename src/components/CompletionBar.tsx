d2eb038baf77898977d8e9af1d79b0208aaa52e8
Jaari <jaari.rey@gmail.com>
2023-10-07 23:53:41 +1000

    hide completion bar on results page

--- src/components/CompletionBar.tsx
+++ src/components/CompletionBar.tsx
@@ -3,13 +3,14 @@ import "../styles/componentStyles/completion-bar.scss";
 
 
 export interface CompletionBarProps {
-	testCompletionPercentage: number
+	testCompletionPercentage: number,
+	showResultsComponent: boolean
 }
 
-const CompletionBar = ({testCompletionPercentage}: CompletionBarProps) => {
+const CompletionBar = ({testCompletionPercentage, showResultsComponent}: CompletionBarProps) => {
 	
 	return (
-		<div style={{width: testCompletionPercentage.toString() + "%"}} className="completion-bar"></div>
+		<div style={{width: testCompletionPercentage.toString() + "%", opacity: showResultsComponent ? 0 : 1}} className="completion-bar"></div>
 	);
 };
 
