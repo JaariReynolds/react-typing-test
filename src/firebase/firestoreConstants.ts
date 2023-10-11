352a8b332e606f4be4ab601f30ba6d6a82cc1725
Jaari <jaari.rey@gmail.com>
2023-10-12 00:04:12 +1000

    added gets for highscores

--- src/firebase/firestoreConstants.ts
+++ src/firebase/firestoreConstants.ts
@@ -2,3 +2,5 @@ import { collection } from "firebase/firestore";
 import { database } from "./firebase";
 
 export const usersCollectionRef = collection(database, "users");
+export const wordCountScoresCollectionRef = collection(database, "wordCountScores");
+export const timedScoresCollectionRef = collection(database, "timedScores");
--- src/firebase/firestoreGet.ts
+++ src/firebase/firestoreGet.ts
@@ -1,6 +1,8 @@
 import { database } from "./firebase";
-import { DocumentData, doc, getDoc, getDocs, query, where } from "firebase/firestore";
-import { usersCollectionRef } from "./firestoreConstants";
+import { DocumentData, doc, getDoc, getDocs, limit, orderBy, query, where } from "firebase/firestore";
+import { timedScoresCollectionRef, usersCollectionRef, wordCountScoresCollectionRef } from "./firestoreConstants";
+import { TestType } from "../App";
+import { TimedScoreDocument, WordCountScoreDocument } from "./firestoreDocumentInterfaces";
 
 export const getAllUsers = async () => {
 	try {
@@ -74,6 +76,56 @@ export const isUsernameAvailable = async (username: string): Promise<boolean> =>
 	} catch (error) {
 		console.error(error);
 		return false;
+	}
+};
+
+// gets the top 10 highscores for the respective test type and test length
+export const getHighScores = async (testType: TestType, testLength: number): Promise<TimedScoreDocument[] | WordCountScoreDocument[]> => {
+	switch (testType) {
+	case TestType.Time:
+		return getHighScoresForTimedTest(testLength);
+	case TestType.Words:
+		return getHighScoresForWordCountTest(testLength);
+	}
+};
+
+const getHighScoresForWordCountTest = async (wordCount: number) => {
+	try {
+		const highScoresQuery = query(wordCountScoresCollectionRef, where("wordCount", "==", wordCount), orderBy("wpm", "desc"), limit(10));
+		const data = await getDocs(highScoresQuery);
+
+		if (data.empty)
+			throw new Error("unable to retrieve highscores");
+
+		const wordCountScoreDocumentArray: WordCountScoreDocument[] = [];
+		data.forEach((doc) => {
+			wordCountScoreDocumentArray.push(doc.data() as WordCountScoreDocument);
+		});
+
+		return wordCountScoreDocumentArray;
+	} catch (error) {
+		console.error(error);
+		return [];
+	}
+};
+
+const getHighScoresForTimedTest = async (testLengthMilliseconds: number) => {
+	try {
+		const highScoresQuery = query(timedScoresCollectionRef, where("testLengthMilliseconds", "==", testLengthMilliseconds), orderBy("wpm", "desc"), limit(10));
+		const data = await getDocs(highScoresQuery);
+
+		if (data.empty)
+			throw new Error("unable to retrieve highscores");
+
+		const timeScoreDocumentArray: TimedScoreDocument[] = [];
+		data.forEach((doc) => {
+			timeScoreDocumentArray.push(doc.data() as TimedScoreDocument);
+		});
+
+		return timeScoreDocumentArray;		
+	} catch (error) {
+		console.error(error);
+		return [];
 	}
 };
 
