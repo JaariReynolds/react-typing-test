da6100e9d0dfa27dd62975a2f17ddf92c881f166
Jaari <jaari.rey@gmail.com>
2023-10-14 22:45:10 +1000

    added highscore collections

--- src/firebase/firestoreConstants.ts
+++ src/firebase/firestoreConstants.ts
@@ -4,3 +4,5 @@ import { database } from "./firebase";
 export const usersCollectionRef = collection(database, "users");
 export const wordCountScoresCollectionRef = collection(database, "wordCountScores");
 export const timedScoresCollectionRef = collection(database, "timedScores");
+export const wordCountHighScoresCollectionRef = collection(database, "wordCountHighScores");
+export const timedHighScoresCollectionRef = collection(database, "timedHighScores");
