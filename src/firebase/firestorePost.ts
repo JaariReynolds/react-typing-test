173d01ab59e046d1a1a811a3a23460b08477d8dc
Jaari <jaari.rey@gmail.com>
2023-10-14 22:47:40 +1000

    post to/update highscores collection if beaten score

--- src/firebase/firestorePost.ts
+++ src/firebase/firestorePost.ts
@@ -1,8 +1,9 @@
 import { database } from "./firebase";
-import { addDoc, collection, doc, setDoc } from "firebase/firestore";
+import { addDoc, collection, doc, getDocs, limit, query, setDoc, updateDoc, where } from "firebase/firestore";
 import { TimedScoreDocument, UserDocument, WordCountScoreDocument } from "./firestoreDocumentInterfaces";
 import { TestWords } from "../interfaces/WordStructure";
 import { TestType } from "../App";
+import { timedHighScoresCollectionRef, wordCountHighScoresCollectionRef } from "./firestoreConstants";
 
 export const createUserDocument = async (userId: string, email: string, username: string) => {
 	const newUserDocument = doc(database, "users", userId);
@@ -45,6 +46,7 @@ const createTimedScoreDocument = async (username: string, scoreObject: TestWords
 
 	try {
 		await addDoc(timedScoresCollectionRef, newTimedScoreObject);
+		await updateTimedHighScoreDocument(username, newTimedScoreObject);
 	} catch (error) {
 		console.error(error);
 	}
@@ -63,6 +65,83 @@ const createWordCountScoreDocument = async (username: string, scoreObject: TestW
 
 	try {
 		await addDoc(wordCountScoresCollectionRef, newWordCountScoreObject);
+		await updateWordCountHighScoreDocument(username, newWordCountScoreObject);
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+const updateTimedHighScoreDocument = async (username: string, timedScoreObject: TimedScoreDocument) => {
+	try {
+		const highScoreQuery = query(
+			timedHighScoresCollectionRef, 
+			where("username", "==", username), 
+			where("testLengthMilliseconds", "==", timedScoreObject.testLengthMilliseconds), limit(1));
+
+		const data = await getDocs(highScoreQuery);
+	
+		// create new highscore document for user if needed
+		if (data.empty) {
+			await addDoc(timedHighScoresCollectionRef, timedScoreObject);
+			console.log("wordcount highscore doc created!");
+			return;
+		}
+
+		// check if user has beaten their highscore 
+		const currentHighScoreDocument = data.docs[0].data() as TimedScoreDocument;
+		if (currentHighScoreDocument.wpm > timedScoreObject.wpm) {
+			console.log("highscore not beaten, no changes made to collection");
+			return;
+		}
+
+		// if beaten, update document
+		const docRef = doc(database, "timedHighScores", data.docs[0].id);
+		await updateDoc(docRef, {
+			wpm: timedScoreObject.wpm,
+			accuracy: timedScoreObject.accuracy,
+			submissionDate: timedScoreObject.submissionDate
+		});
+
+		console.log("highscore updated!");
+
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+const updateWordCountHighScoreDocument = async (username: string, wordCountScoreObject: WordCountScoreDocument) => {
+	try {
+		const highScoreQuery = query(
+			wordCountHighScoresCollectionRef, 
+			where("username", "==", username), 
+			where("wordCount", "==", wordCountScoreObject.wordCount), limit(1));
+
+		const data = await getDocs(highScoreQuery);
+	
+		// create new highscore document for user if needed
+		if (data.empty) {
+			await addDoc(wordCountHighScoresCollectionRef, wordCountScoreObject);
+			console.log("wordcount highscore doc created!");
+			return;
+		}
+
+		// check if user has beaten their highscore 
+		const currentHighScoreDocument = data.docs[0].data() as WordCountScoreDocument;
+		if (currentHighScoreDocument.wpm > wordCountScoreObject.wpm) {
+			console.log("highscore not beaten, no changes made to collection");
+			return;
+		}
+
+		// if beaten, update document
+		const docRef = doc(database, "wordCountHighScores", data.docs[0].id);
+		await updateDoc(docRef, {
+			wpm: wordCountScoreObject.wpm,
+			accuracy: wordCountScoreObject.accuracy,
+			submissionDate: wordCountScoreObject.submissionDate
+		});
+
+		console.log("highscore updated!");
+
 	} catch (error) {
 		console.error(error);
 	}
