ae75326bfe92461c7fd87e6c195b99e0b55ab447
Jaari <jaari.rey@gmail.com>
2023-10-18 15:17:18 +1000

    moved final testInfo calculations to context

--- src/components/TestResults/TestResults.tsx
+++ src/components/TestResults/TestResults.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-key */
 import "../../styles/componentStyles/typing-test-results.scss";
 
-import React, { useEffect, useRef, useState} from "react";
+import React, { useEffect, useRef} from "react";
 import WpmGraph from "./WpmGraph";
 import { createScoreDocument } from "../../firebase/POST/scorePosts";
 import { useUserContext } from "../../contexts/UserContext";
@@ -9,8 +9,6 @@ import Statistics from "./Statistics";
 import HighScores from "./HighScores";
 import { updateUserSummary } from "../../firebase/POST/userPosts";
 import { useTestInformationContext } from "../../contexts/TestInformationContext";
-import { calculateAccuracy } from "../../functions/calculations/calculateAccuracy";
-import { calculateConsistency } from "../../functions/calculations/calculateConsistency";
 
 export interface TestResultsProps {
    
@@ -21,39 +19,11 @@ export interface TestResultsProps {
 
 const TestResults = ({resultsComponentOpacity, resultsComponentDisplay}: TestResultsProps ) => {
 	const {user, userDocument, isHeaderOpen, setIsHeaderOpen} = useUserContext();
-	const {testInformation, setTestInformation, showResultsComponent, isTestSubmitted, setIsTestSubmitted} = useTestInformationContext();
-
-	const [isCalculationsComplete, setIsCalculationsComplete] = useState<boolean>(false);
+	const {testInformation, showResultsComponent, isTestSubmitted, setIsTestSubmitted, isCalculationsComplete} = useTestInformationContext();
 
 	const isHeaderOpenRef = useRef<boolean>();
 	isHeaderOpenRef.current = isHeaderOpen;
-
-	useEffect(() => {
-		setIsCalculationsComplete(false);
-	}, []);
 	
-	// once results screen shown, calculate extra info to show 
-	useEffect(() => {
-		if (showResultsComponent) {	
-			setIsCalculationsComplete(false);
-
-			const accuracy = calculateAccuracy(testInformation);
-			const consistency = calculateConsistency(testInformation);	
-
-			setTestInformation({
-				...testInformation,
-				accuracy: accuracy,
-				consistency: consistency	
-			});	
-
-			setIsCalculationsComplete(true);
-		}
-		else {
-			setIsCalculationsComplete(false);
-			setIsTestSubmitted(localStorage.getItem("isSubmitted") === "true");
-		}
-	}, [showResultsComponent]);
-
 	// submit score when test hasnt already been submitted, user is logged in, and all result calculations are done
 	useEffect(() => {
 		if (!isTestSubmitted && showResultsComponent && isCalculationsComplete && user && userDocument)
--- src/contexts/TestInformationContext.tsx
+++ src/contexts/TestInformationContext.tsx
@@ -4,6 +4,8 @@ import { TimedScoreDocument, WordCountScoreDocument } from "../firebase/firestor
 import { getHighScores } from "../firebase/GET/scoreGets";
 import { TestInformation } from "../interfaces/WordStructure";
 import { TestType } from "../enums";
+import { calculateAccuracy } from "../functions/calculations/calculateAccuracy";
+import { calculateConsistency } from "../functions/calculations/calculateConsistency";
 
 
 interface TestInformationContextProps {
@@ -13,8 +15,8 @@ interface TestInformationContextProps {
 	testInformation: TestInformation,
 	setTestInformation: React.Dispatch<React.SetStateAction<TestInformation>>,
 	showResultsComponent: boolean,
-	setShowResultsComponent: (bool: boolean) => void
-
+	setShowResultsComponent: (bool: boolean) => void,
+	isCalculationsComplete: boolean
 }
 
 const testInformationInitialState: TestInformation = {
@@ -39,7 +41,8 @@ export const TestInformationContext = createContext<TestInformationContextProps|
 	testInformation: testInformationInitialState,
 	setTestInformation: () => {},
 	showResultsComponent: false,
-	setShowResultsComponent: () => {}
+	setShowResultsComponent: () => {},
+	isCalculationsComplete: false
 });
 
 export const useTestInformationContext = () => {
@@ -55,12 +58,39 @@ export const TestInformationProvider = ({children}: any) => {
 	const [isTestSubmitted, setIsTestSubmitted] = useState<boolean>(localStorage.getItem("isSubmitted") === "true");
 	const [testInformation, setTestInformation] = useState<TestInformation>(testInformationInitialState);
 	const [showResultsComponent, setShowResultsComponent] = useState<boolean>(false);
+
+	const [isCalculationsComplete, setIsCalculationsComplete] = useState<boolean>(false);
+
 	
     
 	useEffect(() => {
 		console.log("TestInformationContext mounted");
 		setIsTestSubmitted(localStorage.getItem("isSubmitted") === "true");
 	}, []);
+
+	// once results screen shown, calculate final info for the TestInformation object
+	useEffect(() => {
+		if (showResultsComponent) {
+			console.log("calculating test results..");
+			setIsCalculationsComplete(false);
+	
+			const accuracy = calculateAccuracy(testInformation);
+			const consistency = calculateConsistency(testInformation);	
+
+			setTestInformation({
+				...testInformation,
+				accuracy: accuracy,
+				consistency: consistency	
+			});	
+
+			setIsCalculationsComplete(true);
+			console.log("test calculations done");
+		}
+		else {
+			setIsCalculationsComplete(false);
+			setIsTestSubmitted(localStorage.getItem("isSubmitted") === "true");
+		}
+	}, [showResultsComponent]);
 
 	const value: TestInformationContextProps = {
 		highScores,
@@ -69,7 +99,8 @@ export const TestInformationProvider = ({children}: any) => {
 		testInformation, 
 		setTestInformation,
 		showResultsComponent,
-		setShowResultsComponent
+		setShowResultsComponent,
+		isCalculationsComplete
 	};
 	
 	return (
