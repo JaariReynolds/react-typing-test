2f31a48bcc1dc178834f0dcd03cf8874368b7b41
Jaari <jaari.rey@gmail.com>
2023-10-23 01:33:27 +1000

    added extra rows to highscores + styling page

--- src/components/TestResults/HighScores.tsx
+++ src/components/TestResults/HighScores.tsx
@@ -1,33 +1,50 @@
 import React from "react";
 import { useTestInformationContext } from "../../contexts/TestInformationContext";
+import { useUserContext } from "../../contexts/UserContext";
+import "../../styles/componentStyles/highscores.scss";
+
 
 const HighScores = () => {
+	const {user} = useUserContext();
 	const {highScores} = useTestInformationContext();
 
-	return (
-		<table>
-			<thead>
-				<tr>
-					<th>username</th>
-					<th>wpm</th>
-					<th>time</th>
-					<th>date</th>
-				</tr>
-			</thead>
-			<tbody>
-				{highScores.map((highscore, index) => {
-					return (
-						<tr key={index}>
-							<td>{highscore.username}</td>
-							<td>{highscore.wpm}</td>
-							<td>{highscore.testLengthSeconds}</td>
-							<td>{highscore.submissionDate.toDate().toDateString()}</td>
+	const renderHighScores = () => {
+		if (user) {
+			return (
+				<table className="highscores-table">
+					<thead>
+						<tr>
+							<th>rank</th>
+							<th>username</th>
+							<th>wpm</th>
+							<th>accuracy</th>
+							<th>consistency</th>
+							<th>time</th>							
 						</tr>
-					);
-				})}
-			</tbody>
-		</table>
-	
+					</thead>
+					<tbody>
+						{highScores.map((highscore, index) => {
+							return (
+								<tr key={index}>
+									<td>{index + 1}</td>
+									<td>{highscore.username}</td>
+									<td>{highscore.wpm}</td>
+									<td>{(highscore.accuracy * 100).toFixed(2)}%</td>
+									<td>{(highscore.consistency * 100).toFixed(2)}%</td>
+									<td>{highscore.testLengthSeconds}s</td>
+								</tr>
+							);
+						})}
+					</tbody>
+				</table>
+			);
+		}
+	};
+
+	return (
+		<div className="highscores-div">
+			{renderHighScores()}
+		</div>	
 	);
 };
 
