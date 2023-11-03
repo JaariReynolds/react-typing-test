db00afee6fa18c04aeab77ad88678e9575e067f2
Jaari <jaari.rey@gmail.com>
2023-11-03 15:17:05 +1000

    function rename to better fit purpose

--- src/components/TestResults/TestResults.tsx
+++ src/components/TestResults/TestResults.tsx
@@ -7,7 +7,7 @@ import { createScoreDocument } from "../../firebase/POST/scorePosts";
 import { useUserContext } from "../../contexts/UserContext";
 import Statistics from "./Statistics";
 import Leaderboard from "./Leaderboard";
-import { updateUserSummary } from "../../firebase/POST/userPosts";
+import { updateUserStatistics } from "../../firebase/POST/userPosts";
 import { useTestInformationContext } from "../../contexts/TestInformationContext";
 
 export interface TestResultsProps {
@@ -48,7 +48,7 @@ const TestResults = ({resultsComponentOpacity, resultsComponentDisplay}: TestRes
 		try {
 			setIsTestSubmitted(false);
 			await createScoreDocument(userDocument!.username, testInformation);
-			await updateUserSummary(user!.uid, testInformation);
+			await updateUserStatistics(user!.uid, testInformation);
 			localStorage.setItem("isSubmitted", "true");
 			setIsTestSubmitted(true);
 		} catch (error) {
