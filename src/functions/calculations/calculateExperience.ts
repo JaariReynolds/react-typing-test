08921fd166211c299e64d9b280acdcb576d22e78
Jaari <jaari.rey@gmail.com>
2023-11-03 15:21:36 +1000

    added formula to calculate experience for each level

--- src/functions/calculations/calculateExperience.ts
+++ src/functions/calculations/calculateExperience.ts
@@ -1,5 +1,14 @@
 import { TestInformation } from "../../interfaces/WordStructure";
 
+const EXPERIENCE_EXPONENT = 1.2;
+
+// amount of experience gained from completing the test provided
 export const calculateExperience = (testInformation: TestInformation): number => {
 	return 49;
 };
+
+// formula to calculate the experience required for the level provided
+export const calculateRequiredExperience = (currentLevel: number) : number => {
+	return Math.ceil((Math.pow(currentLevel, EXPERIENCE_EXPONENT) * 100) / 10) * 10;
+};
+
