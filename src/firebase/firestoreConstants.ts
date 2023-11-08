e85e908b8a5fb02e18785d112353559046430f37
Jaari <jaari.rey@gmail.com>
2023-11-08 16:08:32 +1000

    readonly constants for firestore collection names

--- src/firebase/firestoreConstants.ts
+++ src/firebase/firestoreConstants.ts
@@ -1,8 +1,14 @@
 import { collection } from "firebase/firestore";
 import { database } from "./firebase";
 
-export const usersCollectionRef = collection(database, "users");
-export const wordCountScoresCollectionRef = collection(database, "wordCountScores");
-export const timedScoresCollectionRef = collection(database, "timedScores");
-export const wordCountLeaderboardCollectionRef = collection(database, "wordCountLeaderboard");
-export const timedLeaderboardCollectionRef = collection(database, "timedLeaderboard");
+export const USERS = "users";
+export const WORDCOUNT_SCORES = "wordCountScores";
+export const TIMED_SCORES = "timedScores";
+export const WORDCOUNT_LEADERBOARD = "wordCountLeaderboard";
+export const TIMED_LEADERBOARD = "timedLeaderboard";
+
+export const usersCollectionRef = collection(database, USERS);
+export const wordCountScoresCollectionRef = collection(database, WORDCOUNT_SCORES);
+export const timedScoresCollectionRef = collection(database, TIMED_SCORES);
+export const wordCountLeaderboardCollectionRef = collection(database, WORDCOUNT_LEADERBOARD);
+export const timedLeaderboardCollectionRef = collection(database, TIMED_LEADERBOARD);
