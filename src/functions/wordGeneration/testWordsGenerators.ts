84b1c1b6f8ac5423d98c3279e8562ecab3336968
Jaari <jaari.rey@gmail.com>
2023-11-13 13:08:41 +1000

    added switch case for testMode

--- src/functions/wordGeneration/testWordsGenerators.ts
+++ src/functions/wordGeneration/testWordsGenerators.ts
@@ -1,30 +1,41 @@
-import { TestType } from "../../enums";
+import { TestMode, TestType } from "../../enums";
 import { LetterActiveStatus, TestInformation, Word } from "../../interfaces/WordStructure";
-import { wordsArray } from "../../wordsArray";
+import { emoticonsArray } from "../../WordArrays/emoticonsArray";
+import { standardWordsArray } from "../../WordArrays/standardWordsArray";
 import { punctuationGenerator } from "./punctuationGenerator";
 
 // percentages shouldn't add to more than 1.0 or else you get no "normal" words
 const punctuationPercentage = 0.3;
 const numbersPercentage = 0.05;
-const numberOfRandomWords = wordsArray.length;
+let numberOfRandomWords = 0;
 
 const getRandomInt = (max: number): number => {
 	return Math.floor(Math.random() * max);
 };
 
+const getWordsArray = (testMode: TestMode): string[] => {
+	switch (testMode) {
+	case TestMode.Standard: return standardWordsArray;
+	case TestMode.Emojis: return emoticonsArray;
+	}
+};
+
 // function to randomise the words array 
-export const testWordsGenerator = (testLengthWords: number, includeNumbers: boolean, includePunctuation: boolean, testType: TestType): TestInformation => {
+export const testWordsGenerator = (testLengthWords: number, includeNumbers: boolean, includePunctuation: boolean, testType: TestType, testMode: TestMode): TestInformation => {
 	const randomWordArray: Word[] = [];
+	const wordsArray = getWordsArray(testMode);
 	let randomWord: string | number;
+	numberOfRandomWords = wordsArray.length;
+
 	let characterCount = 0;
 	for (let i = 0; i < testLengthWords; i++) {
 		const randomInt: number = getRandomInt(numberOfRandomWords); // get index for word list
 		const randomNum: number = Math.random();
 
-		if (includeNumbers && randomNum <= numbersPercentage) {
+		if (includeNumbers && testMode === TestMode.Standard && randomNum <= numbersPercentage) {
 			// randomly add numbers to wordArray if needed 
 			randomWord = randomInt.toString();
-		} else if (includePunctuation && randomNum > numbersPercentage && randomNum <= numbersPercentage + punctuationPercentage) {
+		} else if (includePunctuation && testMode === TestMode.Standard && randomNum > numbersPercentage && randomNum <= numbersPercentage + punctuationPercentage) {
 			// randomly add punctuation to strings if needed 
 			randomWord = punctuationGenerator(wordsArray[randomInt]);
 		} else {
