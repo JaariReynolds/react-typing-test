90050ba54f2467c516d5e19048780cba8b137973
Jaari <jaari.rey@gmail.com>
2023-11-14 14:33:22 +1000

    app logo added to app name

--- src/App.tsx
+++ src/App.tsx
@@ -18,8 +18,6 @@ import UpdateCssVariable from "./components/HelperComponents/UpdateCssVariable";
 import Header, { HeaderProps } from "./components/Header/Header";
 import { useUserContext } from "./contexts/UserContext";
 import { useTestInformationContext } from "./contexts/TestInformationContext";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faKeyboard } from "@fortawesome/free-solid-svg-icons";
 
 export const TRANSITION_DELAY = 200;
 
@@ -214,10 +212,10 @@ function App() {
 			<div className="main-container" onMouseMove={handleMouseMove}>
 				<Header {...headerProps}/>
 				<div className="app-name">
-					<FontAwesomeIcon icon={faKeyboard} className="larger-icon-left"/>
+					<img src={process.env.PUBLIC_URL + "/typi.png"} className="app-name-logo"/>
 					typi
 				</div>
-				<img src={process.env.PUBLIC_URL + "/typi.png"} className="app-logo"/>
+				<img src={process.env.PUBLIC_URL + "/typi.png"} className="background-logo"/>
 				<div className="inner-container">
 					<AfkDetectedIndicator {...afkDetectedIndicatorProps}/>
 					<TestOptions />
