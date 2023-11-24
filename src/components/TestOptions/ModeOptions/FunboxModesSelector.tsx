21bdc125af6961756cd2c03d3027ffd301d94209
Jaari <jaari.rey@gmail.com>
2023-11-24 15:04:00 +1000

    extra surrounding div for scrollbar

--- src/components/TestOptions/ModeOptions/FunboxModesSelector.tsx
+++ src/components/TestOptions/ModeOptions/FunboxModesSelector.tsx
@@ -26,65 +26,68 @@ const FunboxModesSelector = ({activeTab}: {activeTab: TestModeTabs}) => {
 
 	return (
 		<div style={{opacity: activeTab === TestModeTabs.Funbox ? 1 : 0}} className={` test-option-selector test-mode-funbox ${interactableStyle}`}>
-			<span className="option-text">
-				<input
-					type="radio"
-					id="medicine"
-					value={TestMode.Medicine}							
-					checked={testMode===TestMode.Medicine}
-					onChange={handleOptionChange}
-					className="hidden-radio-button"
-				/>
-				<label htmlFor="medicine" className="selectable-label">			
-					<FontAwesomeIcon icon={faPrescriptionBottleMedical} className="standard-icon-left"/>				
+			<div className="funbox-scrolling-container">
+				<span className="option-text">
+					<input
+						type="radio"
+						id="medicine"
+						value={TestMode.Medicine}							
+						checked={testMode===TestMode.Medicine}
+						onChange={handleOptionChange}
+						className="hidden-radio-button"
+					/>
+					<label htmlFor="medicine" className="selectable-label">			
+						<FontAwesomeIcon icon={faPrescriptionBottleMedical} className="standard-icon-left"/>				
 					medicine
-				</label>
-			</span>				
+					</label>
+				</span>				
 			
-			<span className="option-text">
-				<input
-					type="radio"
-					id="alphabet"
-					value={TestMode.Alphabet}							
-					checked={testMode===TestMode.Alphabet}
-					onChange={handleOptionChange}
-					className="hidden-radio-button"
-				/>
-				<label htmlFor="alphabet" className="selectable-label">			
-					<FontAwesomeIcon icon={faArrowDownAZ} className="standard-icon-left"/>				
+				<span className="option-text">
+					<input
+						type="radio"
+						id="alphabet"
+						value={TestMode.Alphabet}							
+						checked={testMode===TestMode.Alphabet}
+						onChange={handleOptionChange}
+						className="hidden-radio-button"
+					/>
+					<label htmlFor="alphabet" className="selectable-label">			
+						<FontAwesomeIcon icon={faArrowDownAZ} className="standard-icon-left"/>				
 					alphabet
-				</label>
-			</span>
+					</label>
+				</span>
 
-			<span className="option-text">
-				<input
-					type="radio"
-					id="emojis"
-					value={TestMode.Emojis}							
-					checked={testMode===TestMode.Emojis}
-					onChange={handleOptionChange}
-					className="hidden-radio-button"
-				/>
-				<label htmlFor="emojis" className="selectable-label">			
-					<FontAwesomeIcon icon={faSmileBeam} className="standard-icon-left"/>				
+				<span className="option-text">
+					<input
+						type="radio"
+						id="emojis"
+						value={TestMode.Emojis}							
+						checked={testMode===TestMode.Emojis}
+						onChange={handleOptionChange}
+						className="hidden-radio-button"
+					/>
+					<label htmlFor="emojis" className="selectable-label">			
+						<FontAwesomeIcon icon={faSmileBeam} className="standard-icon-left"/>				
 					emojis
-				</label>
-			</span>	
+					</label>
+				</span>	
 
-			<span className="option-text">
-				<input
-					type="radio"
-					id="countries"
-					value={TestMode.Countries}							
-					checked={testMode===TestMode.Countries}
-					onChange={handleOptionChange}
-					className="hidden-radio-button"
-				/>
-				<label htmlFor="countries" className="selectable-label">			
-					<FontAwesomeIcon icon={faEarthAmericas} className="standard-icon-left"/>				
+				<span className="option-text">
+					<input
+						type="radio"
+						id="countries"
+						value={TestMode.Countries}							
+						checked={testMode===TestMode.Countries}
+						onChange={handleOptionChange}
+						className="hidden-radio-button"
+					/>
+					<label htmlFor="countries" className="selectable-label">			
+						<FontAwesomeIcon icon={faEarthAmericas} className="standard-icon-left"/>				
 					countries
-				</label>
-			</span>				
+					</label>
+				</span>		
+			</div>
+				
 		</div>
 	);
 };
