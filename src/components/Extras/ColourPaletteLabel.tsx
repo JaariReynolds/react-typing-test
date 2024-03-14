27ee5a301feb3821322e748a7e2a4f5fab0f0313
Jaari <jaari.rey@gmail.com>
2024-03-14 12:59:41 +1000

    cursor pointer properly showing on palette hover

--- src/components/Extras/ColourPaletteLabel.tsx
+++ src/components/Extras/ColourPaletteLabel.tsx
@@ -2,18 +2,37 @@ import React from "react";
 import { ColourPaletteStructure } from "../../interfaces/ColourPalettes";
 
 export interface ColourPaletteLabelProps {
-    colourPalette: ColourPaletteStructure,
-    backgroundColour: boolean
+  colourPalette: ColourPaletteStructure;
+  backgroundColour: boolean;
 }
 
-const ColourPaletteLabel = ({colourPalette, backgroundColour}: ColourPaletteLabelProps) => {
-	return (
-		<div style={backgroundColour ? {backgroundColor: colourPalette.backgroundColour} : {}} className="selectable-colour-palette-label">
-			<div style={{backgroundColor: colourPalette.baseFontColour}} className="colour-preview"></div>
-			<div style={{backgroundColor: colourPalette.primaryHighlightColour}} className="colour-preview"></div>
-			<div style={{backgroundColor: colourPalette.secondaryHighlightColour}} className="colour-preview"></div>
-		</div>
-	);
+const ColourPaletteLabel = ({
+  colourPalette,
+  backgroundColour,
+}: ColourPaletteLabelProps) => {
+  return (
+    <div
+      style={
+        backgroundColour
+          ? { backgroundColor: colourPalette.backgroundColour }
+          : {}
+      }
+      className="selectable-colour-palette-label"
+    >
+      <div
+        style={{ backgroundColor: colourPalette.baseFontColour }}
+        className="colour-preview"
+      ></div>
+      <div
+        style={{ backgroundColor: colourPalette.primaryHighlightColour }}
+        className="colour-preview"
+      ></div>
+      <div
+        style={{ backgroundColor: colourPalette.secondaryHighlightColour }}
+        className="colour-preview"
+      ></div>
+    </div>
+  );
 };
 
 export default ColourPaletteLabel;
