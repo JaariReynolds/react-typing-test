1c14a10cb5031938ec69f74df618783ecaafc95d
Jaari <jaari.rey@gmail.com>
2024-03-14 11:59:47 +1000

    cursor properly styled to children on parent hover

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -7,34 +7,51 @@ import { colourPalettes } from "../../interfaces/ColourPalettes";
 import { useUserContext } from "../../contexts/UserContext";
 
 export interface FooterProps {
-	setShowColourPalettes: React.Dispatch<React.SetStateAction<boolean>>,
-	colourPaletteDivRef: React.RefObject<HTMLDivElement>
+  setShowColourPalettes: React.Dispatch<React.SetStateAction<boolean>>;
+  colourPaletteDivRef: React.RefObject<HTMLDivElement>;
 }
 
-const Footer = ({setShowColourPalettes, colourPaletteDivRef}: FooterProps) => {
-	const {selectedPaletteId} = useUserContext();
+const Footer = ({
+  setShowColourPalettes,
+  colourPaletteDivRef,
+}: FooterProps) => {
+  const { selectedPaletteId } = useUserContext();
 
+  const handleShowColourPalettes = () => {
+    if (colourPaletteDivRef.current?.style.maxHeight == "max-content") return;
 
-	const handleShowColourPalettes = () => {
-		if (colourPaletteDivRef.current?.style.maxHeight == "max-content") 
-			return;	
-		
-		setShowColourPalettes(true);
-	};
+    setShowColourPalettes(true);
+  };
 
-	return (
-		<div className="footer">
-			<button className="footer-item palette" onClick={handleShowColourPalettes} tabIndex={-1}>
-				<FontAwesomeIcon icon={faPalette} className="palette-icon standard-icon-left"/>
-				palette
-				<ColourPaletteLabel colourPalette={colourPalettes[selectedPaletteId]} backgroundColour={false} />
-			</button>
-			<a href="https://github.com/JaariReynolds/react-typing-test" target="_blank" title="github.com/JaariReynolds/react-typing-test" className="footer-item" rel="noopener noreferrer">
-				<FontAwesomeIcon icon={faCodeBranch} className="standard-icon-left"/>
-				<span>git v{process.env.REACT_APP_VERSION}</span>
-			</a>		
-		</div>
-	);
+  return (
+    <div className="footer">
+      <button
+        className="footer-item palette"
+        onClick={handleShowColourPalettes}
+        tabIndex={-1}
+      >
+        <FontAwesomeIcon
+          icon={faPalette}
+          className="palette-icon standard-icon-left"
+        />
+        palette
+        <ColourPaletteLabel
+          colourPalette={colourPalettes[selectedPaletteId]}
+          backgroundColour={false}
+        />
+      </button>
+      <a
+        href="https://github.com/JaariReynolds/react-typing-test"
+        target="_blank"
+        title="github.com/JaariReynolds/react-typing-test"
+        className="footer-item"
+        rel="noopener noreferrer"
+      >
+        <FontAwesomeIcon icon={faCodeBranch} className="standard-icon-left" />
+        <span>git v{process.env.REACT_APP_VERSION}</span>
+      </a>
+    </div>
+  );
 };
 
 export default Footer;
