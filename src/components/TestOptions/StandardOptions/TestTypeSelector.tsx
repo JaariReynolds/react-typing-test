1ef81d7a2adf97a9ab25d070491d21b519f49342
Jaari <jaari.rey@gmail.com>
2024-03-14 23:29:30 +1000

    refactored input+label to buttons

--- src/components/TestOptions/StandardOptions/TestTypeSelector.tsx
+++ src/components/TestOptions/StandardOptions/TestTypeSelector.tsx
@@ -7,55 +7,52 @@ import { TestMode, TestType } from "../../../enums";
 import { useTestInformationContext } from "../../../contexts/TestInformationContext";
 
 const TestTypeSelector = () => {
-	const {testType, setTestType, testMode} = useTestInformationContext();
+  const { testType, setTestType, testMode } = useTestInformationContext();
 
-	const handleOptionChange = (event:React.ChangeEvent<HTMLInputElement>) => {
-		setTestType(event.target.value as TestType);
-		localStorage.setItem("testType", event.target.value);
-	};
+  const handleTabClick = (tab: TestType) => {
+    setTestType(tab);
+    localStorage.setItem("testType", tab);
+  };
 
-	return (
-		<div style={{borderColor: testMode === TestMode.Alphabet ? "rgba(0, 0, 0, 0.2" : ""}} className="tab-selector test-option-selector">
-			<div className="option-text">
-				<input
-					type="radio"
-					id="words"
-					value={TestType.Words}
-					name="testType"
-					checked={testType === TestType.Words}
-					onChange={handleOptionChange}
-					className="hidden-radio-button"
-					disabled={testMode === TestMode.Alphabet}
-				/>
-				<label htmlFor="words" className="selectable-label">
-					<FontAwesomeIcon icon={faFont} className="standard-icon-left words-icon"/>
-					{TestType.Words.toString()}
-				</label>
-			</div>
-			<div className="option-text">
-				<input
-					type="radio"
-					id="time"
-					value={TestType.Time}
-					name="testType"
-					checked={testType === TestType.Time}
-					onChange={handleOptionChange}
-					className="hidden-radio-button"
-					disabled={testMode === TestMode.Alphabet}
-				/>
-				<label htmlFor="time" className="selectable-label">
-					<FontAwesomeIcon icon={faClock} className="standard-icon-left"/>
-					{TestType.Time.toString()}				
-				</label>
-			</div>
-			<div 
-				className="tab-selected-underline" 
-				style={{
-					transform: testType === TestType.Words ? "translateX(0%)" : "translateX(100%)", 
-					borderColor: testMode === TestMode.Alphabet ? "rgba(0, 0, 0, 0.2" : ""}}>
-			</div>
-		</div>
-	);
+  return (
+    <div
+      style={{
+        borderColor: testMode === TestMode.Alphabet ? "rgba(0, 0, 0, 0.2" : "",
+      }}
+      className="test-option-selector"
+    >
+      <div className="tab-selector">
+        <button
+          className={testType === TestType.Words ? "tab-selected" : ""}
+          onClick={() => handleTabClick(TestType.Words)}
+        >
+          <FontAwesomeIcon
+            icon={faFont}
+            className="standard-icon-left words-icon"
+          />
+          {TestType.Words.toString()}
+        </button>
+        <button
+          className={testType === TestType.Time ? "tab-selected" : ""}
+          onClick={() => handleTabClick(TestType.Time)}
+        >
+          <FontAwesomeIcon icon={faClock} className="standard-icon-left" />
+          {TestType.Time.toString()}
+        </button>
+        <div
+          className="tab-selected-underline"
+          style={{
+            transform:
+              testType === TestType.Words
+                ? "translateX(0%)"
+                : "translateX(100%)",
+            borderColor:
+              testMode === TestMode.Alphabet ? "rgba(0, 0, 0, 0.2" : "",
+          }}
+        ></div>
+      </div>
+    </div>
+  );
 };
 
 export default TestTypeSelector;
