b6c47f0515e12d21edcb249e5fbdcb69b7e03fae
Jaari <jaari.rey@gmail.com>
2024-03-14 23:29:02 +1000

    cleaner solution to disabled standard options

--- src/components/TestOptions/StandardOptions/StandardOptions.tsx
+++ src/components/TestOptions/StandardOptions/StandardOptions.tsx
@@ -2,15 +2,23 @@ import React from "react";
 import TestTypeSelector from "./TestTypeSelector";
 import TestLengthSecondsSelector from "./TestLengthSecondsSelector";
 import TestLengthWordsSelector from "./TestLengthWordsSelector";
+import { useTestInformationContext } from "../../../contexts/TestInformationContext";
+import { TestMode } from "../../../enums";
 
 const StandardOptions = () => {
-	return (
-		<div className="standard-options-container">
-			<TestTypeSelector />
-			<TestLengthWordsSelector />
-			<TestLengthSecondsSelector />
-		</div>
-	);
+  const { testMode } = useTestInformationContext();
+
+  return (
+    <div
+      className={`standard-options-container ${
+        testMode === TestMode.Alphabet ? "disabled-container" : ""
+      }`}
+    >
+      <TestTypeSelector />
+      <TestLengthWordsSelector />
+      <TestLengthSecondsSelector />
+    </div>
+  );
 };
 
 export default StandardOptions;
